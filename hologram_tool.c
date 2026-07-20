3e30d5d1bd1d8a6d63d29cd52fa1a9c56e632fdb
Michael Listrom <155396848+subunits@users.noreply.github.com>
2026-07-20 17:08:23 -0400

    Implement hologram generation and PNG saving

---
+++ hologram_tool.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fftw3.h>
+#include <png.h>
+
+// Three Gaussians object
+double object(double x, double y) {
+    double r1 = exp(-((x+30)*(x+30) + (y+20)*(y+20)) / 200.0);
+    double r2 = exp(-((x-40)*(x-40) + (y-25)*(y-25)) / 300.0);
+    double r3 = exp(-(x*x + y*y) / 500.0);
+    return r1 + r2 + r3;
+}
+
+int save_png(const char *filename, unsigned char *data, int width, int height) {
+    FILE *fp = fopen(filename, "wb");
+    if (!fp) return 1;
+    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    png_infop info = png_create_info_struct(png);
+    if (setjmp(png_jmpbuf(png))) { png_destroy_write_struct(&png, &info); fclose(fp); return 1; }
+    png_init_io(png, fp);
+    png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_write_info(png, info);
+    png_bytep *row_ptrs = (png_bytep*) malloc(sizeof(png_bytep) * height);
+    for (int y = 0; y < height; y++) row_ptrs[y] = data + y * width;
+    png_write_image(png, row_ptrs);
+    png_write_end(png, NULL);
+    free(row_ptrs);
+    png_destroy_write_struct(&png, &info);
+    fclose(fp);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 7) {
+        printf("Usage: %s NX NY kx ky mode output.png\n", argv[0]);
+        return 1;
+    }
+    int NX = atoi(argv[1]);
+    int NY = atoi(argv[2]);
+    if (NX <= 0 || NY <= 0) { fprintf(stderr, "Invalid dimensions\n"); return 1; }
+    
+    double kx = atof(argv[3]), ky = atof(argv[4]);
+    int mode = atoi(argv[5]);
+
+    fftw_complex *U_obj = fftw_malloc(sizeof(fftw_complex) * NX * NY);
+    fftw_complex *U_fft = fftw_malloc(sizeof(fftw_complex) * NX * NY);
+    fftw_plan plan_fwd = fftw_plan_dft_2d(NY, NX, U_obj, U_fft, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    for (int y = 0; y < NY; y++) {
+        for (int x = 0; x < NX; x++) {
+            U_obj[y*NX + x][0] = object(x - NX/2.0, y - NY/2.0);
+            U_obj[y*NX + x][1] = 0.0;
+        }
+    }
+    fftw_execute(plan_fwd);
+
+    unsigned char *holo = (unsigned char*) malloc((size_t)NX * (size_t)NY);
+    double maxVal = 0.0;
+    double *raw_vals = malloc(sizeof(double) * NX * NY);
+
+    for (int y = 0; y < NY; y++) {
+        for (int x = 0; x < NX; x++) {
+            double re = U_fft[y*NX + x][0], im = U_fft[y*NX + x][1];
+            double phase = kx * x + ky * y;
+            double field_re = re + cos(phase), field_im = im + sin(phase);
+            double val;
+            if (mode == 0) val = field_re*field_re + field_im*field_im;
+            else if (mode == 1) val = sqrt(field_re*field_re + field_im*field_im);
+            else val = (atan2(field_im, field_re) + M_PI) / (2*M_PI);
+            raw_vals[y*NX + x] = val;
+            if (val > maxVal) maxVal = val;
+        }
+    }
+
+    for (int i = 0; i < NX * NY; i++) {
+        holo[i] = (mode == 2) ? (unsigned char)(255.0 * raw_vals[i]) : (maxVal > 0 ? (unsigned char)(255.0 * raw_vals[i] / maxVal) : 0);
+    }
+
+    save_png(argv[6], holo, NX, NY);
+    free(raw_vals); free(holo); fftw_destroy_plan(plan_fwd);
+    fftw_free(U_obj); fftw_free(U_fft);
+    return 0;
+}
